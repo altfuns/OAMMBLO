96be3f4848c20c8484f964aef1d5165b8b0844ec
Luis Alfonso Aguilar-Sánchez <laltfuns@gmail.com>
2012-04-02 20:20:44 -0600

    Implementación de las fucionalidad exepto el layout final(dashboard)

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/OammbloActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/OammbloActivity.java
@@ -1,6 +1,9 @@
 package com.fr4gus.android.oammblo.ui;
 
+import com.fr4gus.android.oammblo.R;
+
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Base Activity class. All activities on this application should inherit from OammbloActivity
--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
@@ -2,6 +2,7 @@ package com.fr4gus.android.oammblo.ui;
 
 import com.fr4gus.android.oammblo.R;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 /**
@@ -12,10 +13,34 @@ import android.os.Bundle;
  */
 public class SplashActivity extends OammbloActivity {
 
+	private int splashTime = 1000;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        thread.start();
+        
     }
+    
+    Thread thread = new Thread(){
+    	@Override
+    	public void run() {
+    		try{
+    			int waited = 0;
+        		while(waited <= splashTime){
+        			Thread.sleep(100);
+        			waited += 100;
+        		}
+    		}catch (Exception e) {
+				
+			}finally{
+				finish();
+				Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+				startActivity(intent);
+				
+			}
+    	};
+    };
 
 }
